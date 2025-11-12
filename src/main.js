8beff080013249cbbebefda0e6b2d0348ec33335
Masoud Tajer <m.tajer@vandar.io>
2025-11-12 10:39:05 +0330

    initial configuration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import './assets/css/font.css';
+import './assets/css/style.css';
+
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
