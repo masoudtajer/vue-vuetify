002cc7a31d4630afdc105ab7fe2349883e0d6c74
Masoud Tajer <m.tajer@vandar.io>
2025-11-16 10:43:14 +0330

    create auth layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthLayout from "@/layouts/AuthLayout.vue";
+import Login from "@/views/Auth/Login.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/auth',
+            component: AuthLayout,
+            children: [
+                {
+                    path: '',
+                    name: 'login',
+                    component: Login
+                }
+            ]
+        },
+    ],
 })
 
 export default router
