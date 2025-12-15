c1ca3f5fcbb5f1d5eee3658024fa15d67f091d85
Masoud Tajer <m.tajer@vandar.io>
2025-12-15 11:24:10 +0330

    add dashboard layout

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,11 +1,38 @@
 <script setup>
+import { ref, watch } from 'vue';
 
+const drawer = ref(true);
 </script>
 
 <template>
   <v-app>
-    <v-main min-height="100vh" class="d-flex justify-center align-center">
+    <v-main min-height="100vh">
+      <v-navigation-drawer location="right" v-model="drawer">
+        <v-list class="pt-0">
+          <v-list-item
+              class="py-3 bg-deep-purple-lighten-4"
+              variant="tonal"
+              prepend-avatar="https://avatars.githubusercontent.com/u/26341440?v=4&quot"
+              subtitle="مدیر سایت"
+              title="توفیق حمزه ئی"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar scroll-threshold="439" color="primary">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>پنل ادمین وب آموز</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-account"></v-btn>
+        </template>
+      </v-app-bar>
       <router-view/>
+      <v-footer class="justify-center py-4" color="primary" app>
+        <p>تمامی حقوق این سایت محفوظ میباشد.</p>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
